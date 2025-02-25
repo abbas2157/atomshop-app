8b93fd73ed069f24be946432e4a3d47431a69458
NuzhatUlAin <nuzhatulain.28@gmail.com>
2025-02-25 22:02:02 +0500

    Profile main page and navigation from home added

--- lib/network/urls/app_urls.dart
+++
@@ -1,8 +0,0 @@
-abstract class AppUrls {
-  static const String baseUrl = "https://atomshop.pk/api/";
-
-  /// endpoints
-  static const String login = "account/login";
-  static const String signup = "account/create";
-  static const String categories = "categories";
-}
